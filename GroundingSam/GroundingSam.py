db77de9193ba370dbeaa93b5a8a98d0c48fd3b7d
MAALOUF_IMAD <maaloufimad0@gmail.com>
2024-11-18 01:54:58 +0100

    Update GroundingSam.py

--- GroundingSam/GroundingSam.py
+++ GroundingSam/GroundingSam.py
@@ -183,88 +183,88 @@ class GroundingSam:
         )
     
     
-def detect_all_objects(self, prompts: list, BOX_THRESHOLD=0.35, TEXT_THRESHOLD=0.25):
-    """
-    Détecte tous les objets dans les images en utilisant une liste de classes données.
-    
-    :param prompts: Une liste de classes à utiliser pour la détection (par exemple, ["guitar", "piano", "person"]).
-    :param BOX_THRESHOLD: Seuil pour la détection des boîtes.
-    :param TEXT_THRESHOLD: Seuil pour la détection des objets par texte.
-    :return: Un dictionnaire contenant les objets détectés pour chaque image.
-    """
-    images = {}
-    annotations = {}
-    detected_objects = {}  # Dictionnaire pour stocker les objets détectés
-
-    for image_path in tqdm(self.image_paths):
-        image_name = image_path.name
-        image_path = str(image_path)
-        image = cv2.imread(image_path)
-
-        # Utiliser les prompts pour la détection
-        detections = grounding_dino_model.predict_with_classes(
-            image=image,
-            classes=prompts,  # Utiliser la liste de classes comme prompt
-            box_threshold=BOX_THRESHOLD,
-            text_threshold=TEXT_THRESHOLD
-        )
-        
-        # Filtrer les détections valides
-        detections = detections[detections.class_id != None]
-        detections.mask = segment(
-            sam_predictor=sam_predictor,
-            image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
-            xyxy=detections.xyxy
-        )
-        
-        images[image_name] = image
-        annotations[image_name] = detections
-        
-        # Ajouter les objets détectés à la liste
-        detected_objects[image_name] = [
-            {
-                "label": self.classes[class_id],
-                "confidence": confidence,
-                "bounding_box": bounding_box
-            }
-            for _, _, confidence, class_id, bounding_box in detections
-        ]
-
-    # Annoter et afficher les images détectées
-    
-
-    return detected_objects  
-
-def annotate_images_with_prompt(self, images: dict, annotations: dict):
-    plot_images = []
-    plot_titles = []
-
-    box_annotator = sv.BoxAnnotator()
-    mask_annotator = sv.MaskAnnotator()
-
-    for image_name, detections in annotations.items():
-        image = images[image_name]
-        plot_images.append(image)
-        plot_titles.append(image_name)
-
-        labels = [
-            f"{self.classes[class_id]} {confidence:0.2f}" 
-            for _, _, confidence, class_id, _ 
-            in detections
-        ]
-        annotated_image = mask_annotator.annotate(scene=image.copy(), detections=detections)
-        annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
-        plot_images.append(annotated_image)
-        title = " ".join(set([
-            self.classes[class_id]
-            for class_id
-            in detections.class_id
-        ]))
-        plot_titles.append(title)
-
-    sv.plot_images_grid(
-        images=plot_images,
-        titles=plot_titles,
-        grid_size=(len(annotations), 2),
-        size=(2 * 4, len(annotations) * 4)
-    )
+  def detect_all_objects(self, prompts: list, BOX_THRESHOLD=0.35, TEXT_THRESHOLD=0.25):
+      """
+      Détecte tous les objets dans les images en utilisant une liste de classes données.
+      
+      :param prompts: Une liste de classes à utiliser pour la détection (par exemple, ["guitar", "piano", "person"]).
+      :param BOX_THRESHOLD: Seuil pour la détection des boîtes.
+      :param TEXT_THRESHOLD: Seuil pour la détection des objets par texte.
+      :return: Un dictionnaire contenant les objets détectés pour chaque image.
+      """
+      images = {}
+      annotations = {}
+      detected_objects = {}  # Dictionnaire pour stocker les objets détectés
+  
+      for image_path in tqdm(self.image_paths):
+          image_name = image_path.name
+          image_path = str(image_path)
+          image = cv2.imread(image_path)
+  
+          # Utiliser les prompts pour la détection
+          detections = grounding_dino_model.predict_with_classes(
+              image=image,
+              classes=prompts,  # Utiliser la liste de classes comme prompt
+              box_threshold=BOX_THRESHOLD,
+              text_threshold=TEXT_THRESHOLD
+          )
+          
+          # Filtrer les détections valides
+          detections = detections[detections.class_id != None]
+          detections.mask = segment(
+              sam_predictor=sam_predictor,
+              image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
+              xyxy=detections.xyxy
+          )
+          
+          images[image_name] = image
+          annotations[image_name] = detections
+          
+          # Ajouter les objets détectés à la liste
+          detected_objects[image_name] = [
+              {
+                  "label": self.classes[class_id],
+                  "confidence": confidence,
+                  "bounding_box": bounding_box
+              }
+              for _, _, confidence, class_id, bounding_box in detections
+          ]
+  
+      # Annoter et afficher les images détectées
+      
+  
+      return detected_objects  
+  
+  def annotate_images_with_prompt(self, images: dict, annotations: dict):
+      plot_images = []
+      plot_titles = []
+  
+      box_annotator = sv.BoxAnnotator()
+      mask_annotator = sv.MaskAnnotator()
+  
+      for image_name, detections in annotations.items():
+          image = images[image_name]
+          plot_images.append(image)
+          plot_titles.append(image_name)
+  
+          labels = [
+              f"{self.classes[class_id]} {confidence:0.2f}" 
+              for _, _, confidence, class_id, _ 
+              in detections
+          ]
+          annotated_image = mask_annotator.annotate(scene=image.copy(), detections=detections)
+          annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+          plot_images.append(annotated_image)
+          title = " ".join(set([
+              self.classes[class_id]
+              for class_id
+              in detections.class_id
+          ]))
+          plot_titles.append(title)
+  
+      sv.plot_images_grid(
+          images=plot_images,
+          titles=plot_titles,
+          grid_size=(len(annotations), 2),
+          size=(2 * 4, len(annotations) * 4)
+      )
