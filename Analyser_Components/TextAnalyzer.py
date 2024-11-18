58791897da2d31dbcdd956df15ac83a6d8342c5c
imadmaalouf02 <maaloufimad0@gmail.com>
2024-11-18 14:50:32 +0100

    commit

---
+++ Analyser_Components/TextAnalyzer.py
@@ -0,0 +1,29 @@
+class TextAnalyzer:
+    def __init__(self, text, keywords):
+        self.text = text
+        self.keywords = keywords
+        self.negation_words = ['not', 'no', 'never', 'none', 'nobody', 'nothing', 'neither', 'nor', 
+                               "don't", "doesn't", "can't", "won't", "isn't", "aren't", "wasn't", "weren't"]
+    
+    def analyze(self):
+        results = {}
+        words = self.text.split()
+        last_index = -1  # Pour suivre la position du dernier mot-clé analysé
+        
+        for keyword in self.keywords:
+            if keyword in words:
+                index = words.index(keyword, last_index + 1)  # Commencer la recherche après le dernier mot-clé
+                negation_found = False
+                
+                # Vérifier les mots de négation avant le mot-clé
+                for i in range(last_index + 1, index):
+                    if words[i] in self.negation_words:
+                        negation_found = True
+                        break
+                
+                results[keyword] = 'negation' if negation_found else 'no comment'
+                last_index = index  # Mettre à jour l'index du dernier mot-clé analysé
+            else:
+                results[keyword] = 'not found'
+        
+        return results
